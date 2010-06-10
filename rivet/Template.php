2a48898a1cd27729e576ebb535d4cc508d73e7f7
Justin Morris <justin@pixelbloom.com>
2010-06-10 19:41:10 +1000

    Updates to the Templating and template url reverse tag

--- rivet/Template.php
+++ rivet/Template.php
@@ -3,7 +3,7 @@
 	require_once('Twig/lib/Twig/Autoloader.php');
 	Twig_Autoloader::register();
 	
-	// Fix this... eww...
+	// todo: Fix this... eww...
 	require_once('TwigExtensions/rivet-url/twig-url.php');
 	
 	class Template {
@@ -15,12 +15,13 @@
 		
 		private static function init(){
 		    self::$_instance = new self();
-		    $loader = new Twig_Loader_Filesystem(BASE_PATH.'/templates');
-		    self::$_instance->twig = new Twig_Environment($loader, array(
+		    self::$_instance->twig = new Twig_Environment(new Twig_Loader_Filesystem(BASE_PATH.'/templates'), array(
 		    	'debug' => TRUE,
+		    	'base_template_class' => Base_Template,
 		    	'cache' => BASE_PATH.'/rivet/cache'
 		    ));
 		    self::$_instance->twig->addExtension(new URL_Extension());
+		    self::$_instance->twig->addExtension(new Twig_Extension_Escaper(true));
 		}
 		
 		public static function getInstance(){
@@ -31,9 +32,12 @@
 		    if( self::$_instance === NULL )
 		        self::init();
 		    
-		    $template = self::$_instance->twig->loadTemplate($path);
 		    $args['request'] = Request::getInstance();
 		    $args['static_url'] = Rivet::getInstance()->config['static_url'];
+		    
+		    $template = self::$_instance->twig->loadTemplate($path);
+		    $template->routes = Routes::getInstance();
+		    
 		    self::$_instance->body = $template->render($args);
 		    return self::$_instance->body;
 		}
@@ -42,3 +46,41 @@
 			return $this->body;
 		}
 	}
+	
+	
+	abstract class Base_Template extends Twig_Resource implements Twig_TemplateInterface{
+	
+		private $data = array();
+		
+		public function __set($name, $value) {
+	        $this->data[$name] = $value;
+	    }
+	
+	    public function __get($name) {
+	        if (array_key_exists($name, $this->data)) {
+	            return $this->data[$name];
+	        }
+	        $trace = debug_backtrace();
+	        trigger_error(
+	            'Undefined property via __get(): ' . $name .
+	            ' in ' . $trace[0]['file'] .
+	            ' on line ' . $trace[0]['line'],
+	            E_USER_NOTICE);
+	        return null;
+	    }
+	    
+	    public function render(array $context){
+	        ob_start();
+	        try{
+		          $this->display($context);
+	        }catch (Exception $e){
+		          ob_end_clean();
+		          throw $e;
+	        }
+	        return ob_get_clean();
+		}
+		
+		abstract protected function getName();
+	}
+	
+	
